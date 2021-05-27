daa21cd3eef94770a8db212135dc983b4a865921
geotrav <tloseke@gmail.com>
2021-05-27 14:36:48 -0500

    added lines

--- Mapping_Lines/static/js/logic.js
+++ Mapping_Lines/static/js/logic.js
@@ -1,7 +1,7 @@
 // Add console.log to check to see if our code is working.
 console.log("working");
 // Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([34.0522, -118.2437], 4);
+let map = L.map('mapid').setView([37.6213, -122.3790], 5);
 // // Create the map object with a center and zoom level. Optional method for multiple tile layers
 // let map = L.map("mapid", {
 //     center: [
@@ -9,6 +9,22 @@ let map = L.map('mapid').setView([34.0522, -118.2437], 4);
 //     ],
 //     zoom: 4
 //   });
+// Coordinates for each point to be used in the line.
+let line = [
+    [33.9416, -118.4085],
+    [37.6213, -122.3790],
+    [30.1975, -97.6664],
+    [43.6777, -79.6248],
+    [40.6413,-73.7781]
+  ];
+  // Create a polyline using the line coordinates and make the line red.
+L.polyline(line, {
+    color: "blue",
+    opacity: 0.5,
+    weight: 4,
+    dashArray: '10,10'
+
+  }).addTo(map);
 // Get data from cities.js
 let cityData = cities;
 // Loop through the cities array and create one marker for each city.
@@ -24,7 +40,7 @@ cityData.forEach(function(city) {
 });
 // We create the tile layer that will be the background of our map.
 
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
