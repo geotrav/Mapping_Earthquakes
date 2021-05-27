fa448c778a5d284a53239e45f6482d698e4a1267
geotrav <tloseke@gmail.com>
2021-05-27 11:34:48 -0500

    Multiple markers

--- Mapping_Multiple_Points/static/js/logic.js
+++ Mapping_Multiple_Points/static/js/logic.js
@@ -1,7 +1,7 @@
 // Add console.log to check to see if our code is working.
 console.log("working");
 // Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([34.0522, -118.2437], 14);
+let map = L.map('mapid').setView([34.0522, -118.2437], 4);
 // // Create the map object with a center and zoom level. Optional method for multiple tile layers
 // let map = L.map("mapid", {
 //     center: [
@@ -9,13 +9,19 @@ let map = L.map('mapid').setView([34.0522, -118.2437], 14);
 //     ],
 //     zoom: 4
 //   });
-//  Add a marker to the map for Los Angeles, California.
-L.circleMarker([34.0522, -118.2437], {
-    color: "black",
-    fillColor: "#ffffa1",
-    radius: 300
- }).addTo(map);
-
+// Get data from cities.js
+let cityData = cities;
+// Loop through the cities array and create one marker for each city.
+cityData.forEach(function(city) {
+    console.log(city)
+    L.circleMarker(city.location, {
+        radius: city.population/200000,
+        color: 'orange',
+        weight: '4'
+    })
+    .bindPopup("<h2>"+ city.city + ", " + city.state + "<h2> <hr> <h3>Population " +city.population.toLocaleString() + "</h3>")
+.addTo(map);
+});
 // We create the tile layer that will be the background of our map.
 
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
