b74be1e2d051e8c2c24ae2c04b3bbf267fb50ff8
geotrav <tloseke@gmail.com>
2021-05-27 17:33:44 -0500

    added flight nonstop lines

--- Mapping_GeoJSON_Linestrings/static/js/logic.js
+++ Mapping_GeoJSON_Linestrings/static/js/logic.js
@@ -16,30 +16,42 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
 });
 // Create a base layer that holds both maps.
 let baseMaps = {
-    Street: streets,
-    Dark: dark
+    Day_Navigation: light,
+    Night_Navigation: dark
 };
 // Create the map object with center, zoom level and default layer.
 let map = L.map('mapid', {
-    center: [30, 30],
+    center: [44.0, -80.0],
     zoom: 2,
-    layers: [streets]
+    layers: [light]
 })
 // Pass our map layers into our layers control and add the layers control to the map.
 L.control.layers(baseMaps).addTo(map);
 // Then we add our "graymap" tile layer to the map
 //streets.addTo(map);
 // Accessing the airport GeoJSON URL
-let airportData = "https://raw.githubusercontent.com/geotrav/Mapping_Earthquakes/Mapping_GeoJSON_Points/majorAirports.json";
+let torontoData = "https://raw.githubusercontent.com/geotrav/Mapping_Earthquakes/Mapping_GeoJSON_Linestrings/torontoRoutes.json";
+// Create a style for the lines.
+let myStyle = {
+    color: "#ffffa1",
+    weight: 2
+}
+
 // Grabbing our GeoJSON data.
-d3.json(airportData).then(function(data) {
+d3.json(torontoData).then(function(data) {
     console.log(data);
   // Creating a GeoJSON layer with the retrieved data.
   L.geoJson(data, {
-     onEachFeature: function(feature, layer) {
-             console.log(layer);
-             layer.bindPopup("<h2> Airport Code : " + feature.properties.faa + "<h2> <hr> <h3> Airport Name : " + feature.properties.name + "</h3>");
-         return layer.feature.properties.description;
-    }
-  }).addTo(map);
+        style: myStyle,
+      onEachFeature: function(feature,layer) {
+          layer.bindPopup("<h3> Airline: " + feature.properties.airline + "</h3> <hr> <h3> Destination: "
+          + feature.properties.dst + "</h3>");
+      }
+  }).addTo(map); 
+    //  onEachFeature: function(feature, layer) {
+    //          console.log(layer);
+    //          layer.bindPopup("<h2> Airport Code : " + feature.properties.faa + "<h2> <hr> <h3> Airport Name : " + feature.properties.name + "</h3>");
+    //      return layer.feature.properties.description;
+    //}
+  //}).addTo(map);
 });
